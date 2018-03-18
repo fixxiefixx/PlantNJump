55132daebce9a0d3398aba0ef1e1e6d4eb947ead
fixx <fixx@hotmail.de>
2018-03-18 22:55:33 +0100

    ADDED: Türen die automatisch aufgehen wenn sich der Spieler einem bestimmten Ring festhält.

---
+++ Assets/Resources/Scripts/HangTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HangTrigger : MonoBehaviour {
+
+    public GameObject ObjectToTrigger = null;
+
+    void OnPlayerHang()
+    {
+        if (ObjectToTrigger != null)
+        {
+            ObjectToTrigger.SendMessage("OnPlayerHang", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+
+    void OnPlayerUnhang()
+    {
+        if (ObjectToTrigger != null)
+        {
+            ObjectToTrigger.SendMessage("OnPlayerUnhang", SendMessageOptions.DontRequireReceiver);
+        }
+    }
+}
---
+++ Assets/Resources/Scripts/VerticalDoor.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalDoor : MonoBehaviour {
+    public Transform OpenTrans;
+    [Range(0.01f, 100)]
+    public float OpeningTime = 0.5f;
+
+    [Range(0.01f, 100)]
+    public float ClosingTime = 0.2f;
+
+    public bool OpenAtStart = false;
+
+    private Vector3 startPos = Vector3.zero;
+    private float openState = 0;
+    private bool moving = false;
+    private bool opening = false;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        if(OpenAtStart)
+        {
+            transform.position = OpenTrans.position;
+            openState = 1;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (moving)
+        {
+            if (opening)
+            {
+                float speed = 1 / OpeningTime;
+                openState += speed * Time.deltaTime;
+                if (openState >= 1)
+                {
+                    openState = 1;
+                    moving = false;
+                }
+            }
+            else
+            {
+                float speed = 1 / ClosingTime;
+                openState -= speed * Time.deltaTime;
+                if(openState<=0)
+                {
+                    openState = 0;
+                    moving = false;
+                }
+            }
+            transform.position = Vector3.Lerp(startPos, OpenTrans.position, openState);
+        }
+	}
+
+    private void Open()
+    {
+        moving = true;
+        opening = true;
+    }
+
+    private void Close()
+    {
+        moving = true;
+        opening = false;
+    }
+
+    void OnPlayerHang()
+    {
+        if (OpenAtStart)
+        {
+            Close();
+        }
+        else
+        {
+            Open();
+        }
+    }
+
+    void OnPlayerUnhang()
+    {
+        if (OpenAtStart)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+}
